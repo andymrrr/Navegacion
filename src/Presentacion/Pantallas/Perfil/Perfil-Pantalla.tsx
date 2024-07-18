68249e1a3ac2c0b87ba4ba758e79f22aa814ce75
andymrrr <42513348+andymrrr@users.noreply.github.com>
2024-07-18 07:34:52 -0400

    Finalizando Drawer

--- src/Presentacion/Pantallas/Perfil/Perfil-Pantalla.tsx
+++ src/Presentacion/Pantallas/Perfil/Perfil-Pantalla.tsx
@@ -1,12 +1,22 @@
+import { DrawerActions, useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { BotonPrimario } from '../../Componentes/Boton-Primario'
 
 export const PerfilPantalla = () => {
+  const {top} = useSafeAreaInsets()
+  const navegacion = useNavigation();
   return (
     <View>
-        <Text>
-        PerfilPantalla
+        <Text style={{flex:1, paddingHorizontal: 20, marginTop: top + 20}}>
+          PerfilPantalla
         </Text>
+
+        <BotonPrimario
+          label='Abrir Menu'
+          onPress={ ()=> navegacion.dispatch(DrawerActions.toggleDrawer)}
+        />
     </View>
   )
 }
